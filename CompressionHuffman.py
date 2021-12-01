23c95016ac464dcc6a83e999feab2206c9612e7c
lzlocki <lzlocki@poczta.fm>
2021-12-01 21:49:10 +0100

    Add bit mask calculation

--- CompressionHuffman.py
+++ CompressionHuffman.py
@@ -1,6 +1,8 @@
 # LZ 76103
 # Compression Huffman
 
+import math
+
 table = [0 for element in range(127)]
 
 
@@ -14,19 +16,39 @@ def update_table(possition):
     table[position] = 1
 
 
+def give_me_table_sum(table):
+    sum = 0
+    for e in table:
+        sum += e
+    return sum
 
+
+def calculating_N_bits_to_create_mask(int_signs_quantity):
+    n = math.ceil(math.log(int_signs_quantity,2))
+    return n
 
 
 # Main program
 
+L = 0   # signs quantity
+X = 0   # different signs
+N = 0   # bits numbers needed to create mask of signs
+
 string = "AABBABBC" # string to compress
 
-print("String to compress:")
-print(string)
+print(f"String to compress: {string}")
 
 for el in string:
+    L += 1
     position = give_me_int_base_on_char(el)
     update_table(position)
+print(f"Calculated signs quantity L: {L}")
 
-print("Table with letters")
-print(table)
+X = give_me_table_sum(table)
+print(f"calculated different signs X: {X}")
+
+N = calculating_N_bits_to_create_mask(give_me_table_sum(table))
+print(f"Calculated bits needed to create mask N: {N}")
+
+char = chr(X)
+print(f"char based on different signs quantity : {char}")
